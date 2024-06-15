813b19a98efbdfebdee4fe39f5e74e7f522a21e6
Arthur Andrade <git@arthur404.dev>
2024-06-15 01:15:45 -0300

    feat: add floating navbar and data file ref (#2)
    * feat: add floating navbar and data file ref
    
    * chore: add pull request template

---
+++ src/data/index.tsx
@@ -0,0 +1,26 @@
+import { FaDiscord, FaGithub, FaLinkedin } from "react-icons/fa";
+
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const socialLinks = [
+  {
+    name: "GitHub",
+    link: "https://l.arthur404.dev/forja-github",
+    icon: <FaGithub />,
+  },
+  {
+    name: "LinkedIn",
+    link: "https://l.arthur404.dev/forja-linkedin",
+    icon: <FaLinkedin />,
+  },
+  {
+    name: "Discord",
+    link: "https://l.arthur404.dev/forja-discord",
+    icon: <FaDiscord />,
+  },
+];
