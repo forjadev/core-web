928f56af225a1a53c395ba9376f795f3bcb55c3b
Arthur Andrade <git@arthur404.dev>
2024-06-15 01:03:22 -0300

    feat: add floating navbar and data file ref

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Hero } from "@/components";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import { navItems, socialLinks } from "@/data";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="relative mx-auto flex flex-col items-center justify-center overflow-hidden px-5 sm:px-10">
-      <div className="w-full max-w-7xl">
+      <FloatingNav navItems={navItems} socialLinks={socialLinks} />
+      <div className="container w-full">
         <Hero />
       </div>
     </main>
