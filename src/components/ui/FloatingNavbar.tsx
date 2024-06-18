61aa94cfc11bfb00f1dc85b946f7d4c033ec3903
Maurício Bruno <MBrunoS@users.noreply.github.com>
2024-06-18 13:20:23 -0300

    prod: 1.0.1 (#7)

--- src/components/ui/FloatingNavbar.tsx
+++ src/components/ui/FloatingNavbar.tsx
@@ -6,31 +6,12 @@ import {
   useMotionValueEvent,
   useScroll,
 } from "framer-motion";
-import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
-import logo from "../../../public/logo.svg";
 import Logo from "./Logo";
+import { SocialLinks } from "./SocialLinks";
 
-const animationVariants = {
-  initial: {
-    opacity: 0,
-    y: -30,
-  },
-  animate: (index: number) => ({
-    opacity: 1,
-    y: 0,
-    transition: {
-      delay: 0.15 * index,
-    },
-  }),
-};
-
-export const FloatingNav = ({
-  navItems,
-  className,
-  socialLinks,
-}: {
+interface FloatingNavProps {
   navItems: {
     name: string;
     link: string;
@@ -42,7 +23,13 @@ export const FloatingNav = ({
     link: string;
     icon?: JSX.Element;
   }[];
-}) => {
+}
+
+export const FloatingNav = ({
+  navItems,
+  className,
+  socialLinks,
+}: FloatingNavProps) => {
   const { scrollYProgress } = useScroll();
 
   const [visible, setVisible] = useState(true);
@@ -79,12 +66,19 @@ export const FloatingNav = ({
         transition={{
           duration: 0.2,
         }}
-        className="container fixed top-0 z-[9000] flex w-full items-center justify-between p-4 sm:py-12 lg:px-0"
+        className="container fixed top-0 z-[9000] flex w-full flex-col items-center gap-y-4 py-6 md:flex-row md:justify-between md:py-12"
       >
-        <Logo className="size-10 text-orange" />
-        <motion.ul
+        <motion.div className="flex w-full items-center justify-between px-5 md:w-fit md:px-0">
+          <Logo className="size-8 text-orange md:size-10" />
+
+          {socialLinks && (
+            <SocialLinks links={socialLinks} className="md:hidden" />
+          )}
+        </motion.div>
+
+        <motion.div
           className={cn(
-            "border-black/.1 inset-x-0 top-10 z-[5000] flex max-w-fit items-center justify-center space-x-4 rounded-lg border bg-gradient-to-r from-surface-dark-crust to-surface-dark-mantle px-10 py-5 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] lg:min-w-fit",
+            "border-black/.1 inset-x-0 top-10 z-[5000] flex max-w-fit items-center justify-center space-x-4 rounded-lg border bg-gradient-to-r from-surface-dark-crust to-surface-dark-mantle px-10 py-5 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] md:min-w-[70vw] lg:min-w-fit",
             className,
           )}
           style={{
@@ -93,46 +87,24 @@ export const FloatingNav = ({
             border: "1px solid rgba(255, 255, 255, 0.125)",
           }}
         >
-          {navItems.map(({ link, name, icon }, idx: number) => (
-            <li className="group relative" key={`link=${idx}`}>
+          {navItems.map((navItem: any, idx: number) => (
+            <div className="group relative" key={`link=${idx}`}>
               <Link
-                href={link}
+                href={navItem.link}
                 className={cn(
-                  "relative flex items-center gap-2 space-x-1 text-neutral-50 hover:text-orange dark:text-neutral-50 hover:dark:text-orange",
+                  "relative flex items-center space-x-1 text-neutral-50 hover:text-orange dark:text-neutral-50 hover:dark:text-orange",
                 )}
               >
-                <span className="block sm:hidden">{icon}</span>
-                <span className="hidden !cursor-pointer text-sm sm:block">
-                  {name}
-                </span>
+                <span className="block sm:hidden">{navItem.icon}</span>
+                <span className="!cursor-pointer text-sm">{navItem.name}</span>
               </Link>
               <span className="absolute -bottom-2 left-1/2 h-1 w-1 rounded-full bg-orange opacity-0 transition-opacity duration-200 before:absolute before:left-1/2 before:top-1/2 before:-translate-x-1/2 before:-translate-y-1/2 before:content-[''] group-hover:opacity-100"></span>
-            </li>
+            </div>
           ))}
-        </motion.ul>
+        </motion.div>
+
         {socialLinks && (
-          <motion.ul className="flex flex-col items-center justify-center gap-3 md:flex-row md:gap-4 xl:gap-6">
-            {socialLinks.map(({ name, link, icon }, idx: number) => (
-              <motion.li
-                className="group relative"
-                key={`link=${idx}`}
-                custom={idx}
-                variants={animationVariants}
-                initial={"initial"}
-                animate={"animate"}
-              >
-                <Link
-                  href={link}
-                  target="_blank"
-                  className={cn(
-                    "relative flex items-center space-x-1 text-neutral-50 hover:text-orange dark:text-neutral-50 hover:dark:text-orange",
-                  )}
-                >
-                  <span className="!cursor-pointer text-xl">{icon}</span>
-                </Link>
-              </motion.li>
-            ))}
-          </motion.ul>
+          <SocialLinks links={socialLinks} className="hidden md:flex" />
         )}
       </motion.nav>
     </AnimatePresence>
